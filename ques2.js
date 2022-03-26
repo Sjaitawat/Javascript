1dd8dd8e8f6913390a56e866b5225cabb15ff2e2
Sjaitawat <68988088+Sjaitawat@users.noreply.github.com>
2022-03-26 15:28:55 +0530

    Create ques2.js

---
+++ ques2.js
@@ -0,0 +1,16 @@
+const points = [40, 100, 1, 5, 25, 10];
+var max =0;
+var min =Infinity;
+for(i=0;i<points.length;i++)
+{
+    if(points[i]>max)
+    {
+        max = points[i];
+    }
+    else if (points[i]<min)
+    {
+        min = points[i];
+    }
+}
+console.log(max);
+console.log(min);
