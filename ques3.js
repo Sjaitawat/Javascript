395bbef2bdded61b7c79202c5da7f507d730205c
Sjaitawat <68988088+Sjaitawat@users.noreply.github.com>
2022-03-26 15:43:41 +0530

    Create ques3.js

---
+++ ques3.js
@@ -0,0 +1,12 @@
+const num = 12345
+function reverseDigits(num) {
+    var rev_num = 0;
+    while(num > 0)
+    {
+        rev_num = rev_num * 10 + num % 10;
+        num = Math.floor(num / 10);
+    }
+    return rev_num;
+}
+
+document.write(reverseDigits(num));
